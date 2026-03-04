13d3fc7af2a49a9a5827567ffcfd089044280161
Ken Arigo <kenarigo@yahoo.com>
2026-03-03 21:50:22 -0800

    Cleaned Up game.cpp and game.h

--- EngineXForStudent/Game/Public/Game.h
+++ EngineXForStudent/Game/Public/Game.h
@@ -25,35 +25,13 @@ public:
 private:
     exEngineInterface* mEngine;
     int                                     mFontID;
-    bool                                    mUp;
-    bool                                    mDown;
-    exVector2                               mTextPosition;
-    std::shared_ptr<Ball>                   mBall_First;
-    std::shared_ptr<Ball>                   mBall_Second;
-    std::shared_ptr<Box>                    mBox;
-    std::shared_ptr<Line>                   mLine;
     std::shared_ptr<HUD>                    mHUD;
     std::shared_ptr<TextRenderComponent>    mHUDText;
     std::shared_ptr<SpinningWheelActor>     mSpinningWheel;
     bool                                    mSpinKey;
     std::shared_ptr<TextFileReader>         mTextFileReader;
     std::string                             winnerFilePath = "winners.txt";
-};
-
-	exEngineInterface*						mEngine;
-
-	int										mFontID;
-
-	bool									mUp;
-	bool									mDown;
-
-	exVector2								mTextPosition;
-	std::shared_ptr<Ball>					mBall_First;
-	std::shared_ptr<Ball>					mBall_Second;
-	std::shared_ptr<Box>					mBox;
-	std::shared_ptr<Line>					mLine;
-	std::shared_ptr<HUD>					mHUD;
-	std::shared_ptr<TextRenderComponent> 	mHUDText;
 	WinnerPopup								mWinnerPopup;
 	ConfettiBurstComponent					mConfettiBurst;
+    exColor                                 mHeaderColor;
 };
